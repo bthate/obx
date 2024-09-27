095764204e97c1808e3b4c3a0363aeaf87607688
Bart Thate <rssbotd@gmail.com>
2024-09-27 13:18:55 +0200

    add face

---
+++ obx/face.py
@@ -0,0 +1,13 @@
+# This file is placed in the Public Domain.
+
+
+"interface"
+
+
+from . import command, object, persist, runtime
+
+
+from .command import *
+from .object  import *
+from .persist import *
+from .runtime import *
