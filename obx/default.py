1f701c8a15820d32aeddcaa7445f669581ae4c81
Bart Thate <rssbotd@gmail.com>
2024-09-28 06:48:05 +0200

    add obx.default

---
+++ obx/default.py
@@ -0,0 +1,28 @@
+# This file is placed in the Public Domain.
+# pylint: disable=R
+
+
+"default values"
+
+
+from . import Object
+
+
+class Default(Object):
+
+    "Default"
+
+    def __getattr__(self, key):
+        return self.__dict__.get(key, "")
+
+
+class Config(Default):
+
+    "Config"
+
+
+def __dir__():
+    return (
+        'Config',
+        'Default',
+    )
