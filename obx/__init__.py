d3878f26828a0cf9af9ea3ef8f10737b97a00ff2
Bart Thate <nixtniet@gmail.com>
2025-04-16 16:36:54 +0200

    update init

--- obx/__init__.py
+++ obx/__init__.py
@@ -4,31 +4,18 @@
 "objects"
 
 
-from .disk   import getpath, ident, read, write
-from .find   import find, fntime, last
 from .object import Object, construct, items, keys, values, update
 from .json   import dumps, loads
-from .path   import Workdir, long, skel, setwd, store, types
 
 
 __all__ = (
-    'Workdir',
     'Object',
     'construct',
     'dumps',
-    'find',
-    'fntime',
-    'ident',
     'items',
     'keys',
-    'last',
     'loads',
-    'long',
     'read',
-    'setwd',
-    'skel',
-    'store',
-    'types',
     'values',
     'update',
     'write'
